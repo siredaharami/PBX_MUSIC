7c8f68baa95bc0ef852ebfd9fad157ae8c51a1bd
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-04-10 22:08:27 +0530

    Add files via upload

---
+++ PBXMUSIC/__init__.py
@@ -0,0 +1,32 @@
+from PBXMUSIC.core.bot import VIP
+from PBXMUSIC.core.dir import dirr
+from PBXMUSIC.core.git import git
+from PBXMUSIC.core.userbot import Userbot
+from PBXMUSIC.misc import dbb, heroku
+from telethon import TelegramClient
+from config import API_ID, API_HASH
+
+from SafoneAPI import SafoneAPI
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = VIP()
+api = SafoneAPI()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
+
+telethn = TelegramClient("PBXMUSIC", API_ID, API_HASH)
---
+++ PBXMUSIC/__main__.py
@@ -0,0 +1,59 @@
+import asyncio
+import importlib
+from sys import argv
+from pyrogram import idle
+from pytgcalls.exceptions import NoActiveGroupCall
+
+import config
+from PBXMUSIC import LOGGER, app, userbot
+from PBXMUSIC.core.call import VIP
+from PBXMUSIC.misc import sudo
+from PBXMUSIC.plugins import ALL_MODULES
+from PBXMUSIC.utils.database import get_banned_users, get_gbanned
+from config import BANNED_USERS
+from PBXMUSIC import telethn
+
+async def init():
+    if (
+        not config.STRING1
+        and not config.STRING2
+        and not config.STRING3
+        and not config.STRING4
+        and not config.STRING5
+    ):
+        LOGGER(__name__).error("ᴀʙᴇ ʟᴏᴠᴅᴇ ʟᴏɢɢᴇʀ ɢʀᴏᴜᴘ ᴍᴇ ᴀᴅᴍɪɴ ᴛᴇʀᴀ ʙᴀᴘ ᴋʀᴇ ɢᴀ ᴋʏᴀ ")
+        
+    await sudo()
+    try:
+        users = await get_gbanned()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+        users = await get_banned_users()
+        for user_id in users:
+            if user_id not in BANNED_USERS:
+                BANNED_USERS.add(user_id)
+    except:
+        pass
+    await app.start()
+    for all_module in ALL_MODULES:
+        importlib.import_module("PBXMUSIC.plugins" + all_module)
+    LOGGER("PBXMUSIC.plugins").info(" ꜱᴏ ꜱᴀᴅ 🥳...")
+    await userbot.start()
+    await VIP.start()
+    await VIP.decorators()
+    LOGGER("PBXMUSIC").info("╔═════ஜ۩۞۩ஜ════╗\n  ♨️ᴍᴀᴅᴇ ʙʏ ᴛᴇᴀᴍ ᴘʙx ♨️\n╚═════ஜ۩۞۩ஜ════╝"
+    )
+    await idle()
+    if len(argv) not in (1, 3, 4):
+        await telethn.disconnect()
+    else:
+        await telethn.run_until_disconnected()
+                
+    await app.stop()
+    await userbot.stop()
+    LOGGER("PBXMUSIC").info("               ╔═════ஜ۩۞۩ஜ════╗\n  ♨️ᴍᴀᴅᴇ ʙʏ ᴛᴇᴀᴍ ᴘʙx ♨️\n╚═════ஜ۩۞۩ஜ════╝")
+    
+
+if __name__ == "__main__":
+    telethn.start(bot_token=config.BOT_TOKEN)
+    asyncio.get_event_loop().run_until_complete(init())
