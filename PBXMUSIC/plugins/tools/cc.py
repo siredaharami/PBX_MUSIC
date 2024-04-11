5825e389599dcf9d868bf7a7573e3681fd5ba37e
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-04-11 19:17:26 +0530

    Create cc.py

---
+++ PBXMUSIC/plugins/tools/cc.py
@@ -0,0 +1,35 @@
+from ... import *
+from pyrogram import *
+from pyrogram.types import *
+
+
+@app.on_message(filters.command(["gen", "ccgen"], [".", "!", "/"]))
+async def gen_cc(client, message):
+    if len(message.command) < 2:
+        return await message.reply_text(
+            "** ᴘʟᴢ ɢɪᴠᴇ ᴍᴇ ᴀ ʙɪɴ ᴛᴏ \nɢᴇɴᴇʀᴀᴛᴇ ᴄᴄ ...**"
+        )
+    try:
+        await message.delete()
+    except:
+        pass
+    aux = await message.reply_text("**ɢᴇ....**")
+    bin = message.text.split(None, 1)[1]
+    if len(bin) < 6:
+        return await aux.edit("**❌ ᴡʀᴏɴɢ ʙɪɴ ❗...**")
+    try:
+        resp = await api.ccgen(bin, 10)
+        cards = resp.liveCC
+        await aux.edit(f"""
+**💠 ᴄᴄ ʟɪᴠᴇ 💠**
+`{cards[0]}`\n`{cards[1]}`\n`{cards[2]}`
+`{cards[3]}`\n`{cards[4]}`\n`{cards[5]}`
+`{cards[6]}`\n`{cards[7]}`\n`{cards[8]}`
+`{cards[9]}`
+**💳 ʙɪɴ ** `{resp.results[0].bin}`
+**⏳ ᴛɪᴍᴇ ᴛᴏᴏᴋ :** `{resp.took}`\n\n"""
+        )
+    except Exception as e:
+        return await aux.edit(f"**Error:** `{e}`")
+
+
