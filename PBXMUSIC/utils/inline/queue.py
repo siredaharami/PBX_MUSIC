17938bf60d37a1bc8f38eb6c134025f8a133e797
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-04-12 09:10:59 +0530

    Update queue.py

--- PBXMUSIC/utils/inline/queue.py
+++ PBXMUSIC/utils/inline/queue.py
@@ -69,9 +69,6 @@ def aq_markup(_, chat_id):
             InlineKeyboardButton(text="II ", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="▢ ", callback_data=f"ADMIN Stop|{chat_id}"),
             InlineKeyboardButton(text="‣‣ ", callback_data=f"ADMIN Skip|{chat_id}"),
-
-        ],
-        [
             InlineKeyboardButton(text="▷ ", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="↺ ", callback_data=f"ADMIN Replay|{chat_id}"),
         ],
@@ -113,9 +110,8 @@ def queuemarkup(_, vidid, chat_id):
             InlineKeyboardButton(
                 text="‣‣ ", callback_data=f"ADMIN Skip|{chat_id}"
             ),
-        ],
-        [
             InlineKeyboardButton(text="▷ ", callback_data=f"ADMIN Resume|{chat_id}"),
+            
             InlineKeyboardButton(text="↺ ", callback_data=f"ADMIN Replay|{chat_id}"),
         ],
         [
