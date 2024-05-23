e7b921979497ee2dcaff1be97d9f6c14d512ac20
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-05-23 17:22:35 +0530

    Update __init__.py

--- PBXMUSIC/__init__.py
+++ PBXMUSIC/__init__.py
@@ -3,8 +3,6 @@ from PBXMUSIC.core.dir import dirr
 from PBXMUSIC.core.git import git
 from PBXMUSIC.core.userbot import Userbot
 from PBXMUSIC.misc import dbb, heroku
-from telethon import TelegramClient
-from config import API_ID, API_HASH
 
 from SafoneAPI import SafoneAPI
 from .logging import LOGGER
@@ -28,5 +26,4 @@ Spotify = SpotifyAPI()
 Resso = RessoAPI()
 Telegram = TeleAPI()
 YouTube = YouTubeAPI()
-
-telethn = TelegramClient("PBXMUSIC", API_ID, API_HASH)
+APP = "@MissBroken_Bot"  # connect music api key "Dont change it"
