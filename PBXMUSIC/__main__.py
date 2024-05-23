bd3bc4bed48ed618925866fdd181a85632d76595
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-05-23 18:37:53 +0530

    Update __main__.py

--- PBXMUSIC/__main__.py
+++ PBXMUSIC/__main__.py
@@ -12,7 +12,6 @@ from PBXMUSIC.plugins import ALL_MODULES
 from PBXMUSIC.utils.database import get_banned_users, get_gbanned
 from config import BANNED_USERS
 
-from PBXMUSIC.plugins.tools.clone import restart_bots
 
 
 async def init():
