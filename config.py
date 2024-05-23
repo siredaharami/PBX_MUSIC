20c7ba9dd15ee745e190d7180829e209fb866405
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-05-23 15:04:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/siredaharami/PBX_MUSIC",
+    "https://github.com/siredaharami/PBXMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
